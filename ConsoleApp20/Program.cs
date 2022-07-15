45a30a2439b7ab11570234a608b3a5d4fdc701ab
cemil <cemiw033@gmail.com>
2022-07-15 22:47:08 +0400

    send processes

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Processes
+{
+    public  class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string sts="";
+            var ipAddress = IPAddress.Parse("192.168.0.109");
+            var port = 63291;
+            var endPoint = new IPEndPoint(ipAddress, port);
+            var listener =new TcpListener(endPoint); 
+            listener.Start();
+            ObservableCollection<string> prcesses =new();
+            
+            foreach (var item in Process.GetProcesses())
+            {   
+                prcesses.Add(item.ProcessName);                
+            }
+
+            while (true)
+            {
+
+                var client = listener.AcceptTcpClient();
+                var stream = client.GetStream();
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        var br = new BinaryReader(stream);
+                        var msgs = br.ReadString();
+                        if (msgs == "List")
+                        {
+                            sts = "List";
+                        }
+                        else if (msgs == "Kill")
+                        {
+                            sts = "Kill";
+                        }
+                        else if (msgs == "Create")
+                        {
+                            sts = "Create";
+                        }
+                        else if (msgs == "Help")
+                        {
+                            sts = "Help";
+                        }
+                        
+                        br = new BinaryReader(stream);
+                        if (sts == "List")
+                        {
+                            var bwr = new BinaryWriter(stream);
+                            var json = JsonConvert.SerializeObject(prcesses, Newtonsoft.Json.Formatting.Indented);
+                            bwr.Write(json);
+                            sts = "";
+                        }
+                        else if (sts == "Create")
+                        {
+                            var br1 = new BinaryReader(stream);
+                            var msgs1 = br.ReadString();
+                            Process.Start(new ProcessStartInfo(msgs1));
+                            sts = "";
+                        }
+                        else if (sts == "Kill")
+                        {
+                            var br1 = new BinaryReader(stream);
+                            var msgs1 = br.ReadString();
+                            var lst=Process.GetProcessesByName(msgs1);
+                            foreach (var proc in lst)
+                            {
+                                proc.Kill();
+                            }
+                            sts = "";
+                        }
+                        else if (sts == "Help")
+                        {
+                            var br1 = new BinaryReader(stream);
+                            var msgs1 = br.ReadString();
+                            Console.WriteLine(Process.GetProcesses().FirstOrDefault(t=>t.ProcessName==msgs1).Id);
+                            sts = "";
+                        }
+                    }
+                }); 
+            
+            }
+
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Cb_act.Items.Add("List");
+            Cb_act.Items.Add("Kill");
+            Cb_act.Items.Add("Create");
+            Cb_act.Items.Add("Help");
+        }
+        public int port ;       
+        public TcpClient client;
+        public IPAddress ip ;
+        public ObservableCollection<string> prcesses { get; set; } = new();
+        public IPEndPoint ep;
+        public BinaryReader br;
+        public BinaryWriter bw;
+        public void LOAD()
+        {
+            try
+            {
+               
+                var msg = "";   
+                var stream=client.GetStream();
+                br = new BinaryReader(stream);
+                msg =br.ReadString();
+                prcesses = JsonConvert.DeserializeObject<ObservableCollection<string>>(msg);
+                foreach (var pr in prcesses)
+                {
+                    lst.Items.Add(pr.ToString());
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ip = IPAddress.Parse(ipp.Text);
+                port = int.Parse(portt.Text.ToString());
+                ep = new IPEndPoint(ip, port);
+                client=new TcpClient();
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    MessageBox.Show("Connected");
+                }
+                else
+                {
+                    MessageBox.Show("Cannot connect.");
+                }
+            }
+            catch (Exception m)
+            {
+
+                MessageBox.Show(m.Message);
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(Cb_act.SelectedItem != null)
+            {
+                if(Cb_act.SelectedItem.ToString() == "List")
+                {
+                    LOAD();
+                }
+                else if(Cb_act.SelectedItem.ToString() == "Create")
+                {
+                    var stream = client.GetStream();
+                    var br = new BinaryWriter(stream);
+                    br.Write(prcName.Text);
+                }
+                else if (Cb_act.SelectedItem.ToString() == "Kill")
+                {
+                    var stream = client.GetStream();
+                    var br = new BinaryWriter(stream);
+                    br.Write(prcName.Text);
+                }
+                else if (Cb_act.SelectedItem.ToString() == "Help")
+                {
+                    var stream = client.GetStream();
+                    var br = new BinaryWriter(stream);
+                    br.Write(prcName.Text);
+                }
+            }
+        }
+
+        private void Cb_act_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var stream = client.GetStream();
+                var br = new BinaryWriter(stream);
+                br.Write(Cb_act.SelectedItem.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
